c081b412905deb1f3cef10516e05c3e3f06c10d6
Peter <peterli2619@gmail.com>
2021-01-12 10:52:28 -0800

    Unity2 C# 2021 1 12

---
+++ GD60-Unity2-L02-FirstBreak/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [Header("Movement")]
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _acceleration = 10f;
+    [SerializeField] private float _turnSpeed = 10f;
+
+    // this is a property, similar to field (normal variable), but we have made it publicly readable, but only settable privately
+    public Vector3 MoveInput {get; private set;}
+    public Vector3 LookDirection {get; private set;}
+
+    public void SetMoveInput(Vector3 input)
+    {
+        // flatten input
+        input.y = 0f;
+        // clamp and set input
+        MoveInput = Vector3.ClampMagnitude(input, 1f);
+    }
+
+    public void SetLookDirection(Vector3 direction)
+    {
+        direction.y = 0f;
+        LookDirection = direction.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        // calculate target velocity and difference from current
+        Vector3 targetVelocity = MoveInput * _speed;
+        Vector3 velocityDiff = targetVelocity - _rigidbody.velocity;
+        velocityDiff.y = 0f;
+
+        // get acceleration towards target velocity
+        Vector3 acceleration = velocityDiff * _acceleration;
+        _rigidbody.AddForce(acceleration);
+
+        // rotate the character
+        Quaternion targetRotation = Quaternion.LookRotation(LookDirection);     // creates a (quaternion) rotation based on an input direction
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed * Time.fixedDeltaTime);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
---
+++ GD60-Unity2-L02-FirstBreak/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterMovement _characterMovement;
+    [SerializeField] private Vector2 _moveInput;
+
+    // callback from PlayerInput component
+    public void OnMove(InputValue value)
+    {
+        // read vector2 value from InputValue
+        _moveInput = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        // find move input based on camera direction
+        Vector3 up = Vector3.up;
+        Vector3 right = Camera.main.transform.right;
+        Vector3 forward = Vector3.Cross(right, up);
+        Debug.DrawRay(Camera.main.transform.position, forward * 3f, Color.green);
+
+        Vector3 localMovement = right * _moveInput.x + forward * _moveInput.y;
+
+        _characterMovement.SetMoveInput(localMovement);
+        _characterMovement.SetLookDirection(forward);
+    }
+}
